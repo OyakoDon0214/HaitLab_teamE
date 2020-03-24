5ce06e322d6f3ec1b0b8bc9c273583b33f399299
OyakoDon0214 <metbyroot@gmail.com>
2020-03-24 14:12:38 +0900

    外部からアクセスできるようにクラス化。またcv2.image->Stringとなるメソッドに改良。

--- ImageToString.py
+++ ImageToString.py
@@ -13,22 +13,36 @@ import pyocr.builders
 
 import argparse
 import cv2
+class Image_to_string:
+    def image_to_string(self,image):
+        tools = pyocr.get_available_tools()
+        if len(tools) == 0:
+            print("No OCR tool found")
+            sys.exit(1)
+    # The tools are returned in the recommended order of usage
+        tool = tools[0]
+        txt = tool.image_to_string(
+            self.cv2pil(image),#opencv型のimageからpil型に変換
+            lang="jpn",
+            builder=pyocr.builders.TextBuilder(tesseract_layout=6)
+        )
+        return txt
+    def cv2pil(self,image):#
+        ''' imageをOpenCV型 -> PIL型にするためのもの'''
+        new_image = image.copy()
+        if new_image.ndim == 2:  # モノクロ
+            pass
+        elif new_image.shape[2] == 3:  # カラー
+            new_image = cv2.cvtColor(new_image, cv2.COLOR_BGR2RGB)
+        elif new_image.shape[2] == 4:  # 透過
+            new_image = cv2.cvtColor(new_image, cv2.COLOR_BGRA2RGBA)
+        new_image = Image.fromarray(new_image)
+        return new_image
 
-def Image_to_string():
-    tools = pyocr.get_available_tools()
-    if len(tools) == 0:
-        print("No OCR tool found")
-        sys.exit(1)
-# The tools are returned in the recommended order of usage
-    tool = tools[0]
-    txt = tool.image_to_string(
-        Image.open('./image/img.jpg'),
-        lang="jpn",
-        builder=pyocr.builders.TextBuilder(tesseract_layout=6)
-    )
-    return txt   
 
-txt = Image_to_string()
+
+image=cv2.imread("trimmed_image.png")
+txt = Image_to_string().image_to_string(image)
 print(txt)
 '''
 # txt is a Python string
